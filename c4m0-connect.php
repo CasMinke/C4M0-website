7a80b2d4fdf87dde90f9231665857d05b02095fb
CasMinke <casminke@gmail.com>
2019-11-18 11:47:41 +0100

    made the rules page

--- c4m0-connect.php
+++ c4m0-connect.php
@@ -1,3 +1,4 @@
+<meta charset="UTF-8">
 <?php
 $servername = "localhost";
 $dbname = "c4m0";
@@ -7,7 +8,7 @@ $password = "";
 try{
     $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+    echo "connectie gelukt";
 }
 catch (PDOException $e)
 {
