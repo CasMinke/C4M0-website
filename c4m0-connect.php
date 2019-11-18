885a6f0b417098ea1b371a38806aca3a9383b7c0
CasMinke <casminke@gmail.com>
2019-11-18 10:34:23 +0100

    first upload
    first commit

---
+++ c4m0-connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$dbname = "c4m0";
+$username = "root";
+$password = "";
+
+try{
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}
+catch (PDOException $e)
+{
+    echo "Connectie mislukt: " . $e->getMessage();
+}
