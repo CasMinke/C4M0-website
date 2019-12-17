97f08d2aef01cb078ea1a53a2d98654102d49f24
CasMinke <casminke@gmail.com>
2019-12-17 13:52:32 +0100

    made it so you cant fill in nothing in the create account form

--- php/createaccount.php
+++ php/createaccount.php
@@ -6,23 +6,27 @@ $pass_md5 = md5($password);
 $duplicate = false;
 
 require_once "c4m0-connect.php";
-$searchfordup = $conn->prepare("select * from gameaccount");
-$searchfordup->execute();
+if (!isset($nickname) || trim($nickname) == '' || !isset($password) || trim($password) == '') {
+    header("location: ../fields-not-filled.html");
+} else {
+    $searchfordup = $conn->prepare("select * from gameaccount");
+    $searchfordup->execute();
 
-foreach ($searchfordup as $search) {
-    if ($nickname == $search["username"]) {
-        $duplicate = true;
-        echo "<script>alert('username already in use'); window.location = '../createaccount.html'</script>";
+    foreach ($searchfordup as $search) {
+        if ($nickname == $search["username"]) {
+            $duplicate = true;
+            echo "<script>alert('the username $nickname is already in use!?!'); window.location = '../createaccount.html'</script>";
+        }
     }
-}
-if ($duplicate == false) {
-    $sql = $conn->prepare("insert into gameaccount values (:id, :username, :password)");
+    if ($duplicate == false) {
+        $sql = $conn->prepare("insert into gameaccount values (:id, :username, :password)");
 
-    $sql->bindParam(":id", $id);
-    $sql->bindParam(":username", $nickname);
-    $sql->bindParam(":password", $pass_md5);
+        $sql->bindParam(":id", $id);
+        $sql->bindParam(":username", $nickname);
+        $sql->bindParam(":password", $pass_md5);
 
-    $sql->execute();
+        $sql->execute();
 
-    header("location: ../accountcreated.html");
+        header("location: ../accountcreated.html");
+    }
 }
