1ea7b07f9a4ad87f61f4f282da4af41607e08455
CasMinke <casminke@gmail.com>
2019-12-17 13:43:50 +0100

    you cant make a username that already exits now

--- php/createaccount.php
+++ php/createaccount.php
@@ -3,15 +3,26 @@ $id = NULL;
 $nickname = $_POST["nickname"];
 $password = $_POST["password"];
 $pass_md5 = md5($password);
+$duplicate = false;
 
 require_once "c4m0-connect.php";
+$searchfordup = $conn->prepare("select * from gameaccount");
+$searchfordup->execute();
 
-$sql = $conn->prepare("insert into gameaccount values (:id, :username, :password)");
+foreach ($searchfordup as $search) {
+    if ($nickname == $search["username"]) {
+        $duplicate = true;
+        echo "<script>alert('username already in use'); window.location = '../createaccount.html'</script>";
+    }
+}
+if ($duplicate == false) {
+    $sql = $conn->prepare("insert into gameaccount values (:id, :username, :password)");
 
-$sql->bindParam(":id",$id);
-$sql->bindParam(":username",$nickname);
-$sql->bindParam(":password",$pass_md5);
+    $sql->bindParam(":id", $id);
+    $sql->bindParam(":username", $nickname);
+    $sql->bindParam(":password", $pass_md5);
 
-$sql->execute();
+    $sql->execute();
 
-header("location: ../accountcreated.html");
+    header("location: ../accountcreated.html");
+}
